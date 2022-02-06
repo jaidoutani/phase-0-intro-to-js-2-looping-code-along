930e68c640f7bbae13af3e474f0762b006af1351
Michael Bowlin <jaidoutani@gmail.com>
2022-02-06 12:04:58 -0600

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Code your solutions in this file
+const names = [];
+
+function writeCards(names, event) {
+    const newArray = [];
+    for (let i = 0; i < names.length; i++) {
+        newArray.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+    }
+    return newArray;
+}
+// console.log(writeCards(["Michael"], "birthday")); 
+
+function countDown(num) {
+    while (num >= 0) {
+        console.log(num);
+        num--
+        debugger;
+    }
+}
+console.log(countDown(15));
